2c929c96561a147dca18aef33093a1507d4fbbcd
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-06 06:20:09 -0800

    fix dependency resolving

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "URI",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0)
+		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
 )
