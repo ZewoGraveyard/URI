da87ec493dd2b21b361a43efc248a3133f98a25f
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-04-29 22:32:41 +0200

    Updated Swift 04-25 (#7)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 6),
     ]
 )
